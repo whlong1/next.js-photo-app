63f3e1140607ebb7fd3f1713b437b28f6d5387a9
whlong1 <whunterlong@gmail.com>
2023-07-27 15:37:37 -0400

    Update auth config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,16 @@
 // Middleware Configuration
-import { authMiddleware } from "@clerk/nextjs"
 
 // This example protects all routes including api/trpc routes.
 // See https://clerk.com/docs/nextjs/middleware for more information.
 
+// For custom sign in pages, don't add them to your publicRoutes. Add them to `.env`
+
 //! Note: Clerk requires this file to be in the root or src directory.
 
+import { authMiddleware } from "@clerk/nextjs"
+
+const publicClientPaths = ["/"]
 const publicAPIPaths = ["/api/hello"]
-const publicClientPaths = ["/", "/sign-in*", "/sign-up*"]
 
 export default authMiddleware({
   publicRoutes: [...publicClientPaths, ...publicAPIPaths]

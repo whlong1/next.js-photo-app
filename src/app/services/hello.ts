98e5669c0d8cef840aa85c316900776628620b44
whlong1 <whunterlong@gmail.com>
2023-07-27 15:37:20 -0400

    Add base url

--- src/app/services/hello.ts
+++ src/app/services/hello.ts
@@ -1,14 +1,14 @@
 import 'server-only'
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
-// Context on the cache prop below:
-// https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#dynamic-data-fetching
-const show = async () => {
+export const show = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/hello", { cache: 'no-store' })
+    const res = await fetch(`${BASE_URL}/api/hello`, { cache: 'no-store' })
     return res.json()
   } catch (error) {
     throw error
   }
 }
 
-export { show }
+// Cache prop:
+// https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#dynamic-data-fetching

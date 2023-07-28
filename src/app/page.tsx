cb8d1be6037593135364f5fa1f61299c52a209b6
whlong1 <whunterlong@gmail.com>
2023-07-28 15:03:34 -0400

    Add profile model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+// Auth
+import { auth } from '@clerk/nextjs'
+
 // Services
 import * as helloService from "@/services/hello"
 
 export default async function Home() {
   const res: { message: string, greetings: [] } = await helloService.show()
   console.log("response:", res)
+
+  const { userId } = auth()
+  console.log(userId)
 
   return (
     <>

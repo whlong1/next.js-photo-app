897206b59fe9b27d74a78b4fcf4cf0ae873afd37
whlong1 <whunterlong@gmail.com>
2023-07-28 15:49:25 -0400

    Add user button to nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link"
+import { UserButton } from "@clerk/nextjs"
 
 const Nav = () => {
   return (
-    <nav>
+    <nav className="flex flex-row">
       <Link href="/">Home</Link>
       <Link href="/sign-up">Sign Up</Link>
       <Link href="/sign-in">Sign In</Link>
+      <UserButton afterSignOutUrl="/" />
     </nav>
   )
 }

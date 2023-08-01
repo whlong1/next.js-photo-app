cb65b3ef112a47f5635a2ff8a4941cde5e99f3ea
whlong1 <whunterlong@gmail.com>
2023-08-01 12:34:38 -0400

    Add redirect to sign in

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,21 @@
 
 // For custom sign in pages, don't add them to your publicRoutes. Add them to `.env`
 
+// With the config below, all api endpoints are public.
+
 //! Note: Clerk requires this file to be in the root or src directory.
 
 import { authMiddleware } from "@clerk/nextjs"
+import { redirectToSignIn } from '@clerk/nextjs'
 
 // Add 'debug: true' to view logs:
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/about"],
   afterAuth(auth, req, evt) {
     console.log("After Auth")
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url })
+    }
   }
 })
 

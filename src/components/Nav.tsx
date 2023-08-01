b4541eacf7f51ecb0aaf9df447355a9bcbf2a5ed
whlong1 <whunterlong@gmail.com>
2023-08-01 11:53:59 -0400

    Add protected links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link"
-import { UserButton } from "@clerk/nextjs"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 
 const Nav = () => {
   return (
     <nav className="flex flex-row">
       <Link href="/">Home</Link>
-      <Link href="/sign-up">Sign Up</Link>
-      <Link href="/sign-in">Sign In</Link>
-      <UserButton afterSignOutUrl="/" />
+
+      <SignedOut>
+        <Link href="/sign-up">Sign Up</Link>
+        <Link href="/sign-in">Sign In</Link>
+      </SignedOut>
+
+      <SignedIn>
+        <Link href="/videos">Videos</Link>
+      </SignedIn>
+
+      <SignedIn>
+        <UserButton afterSignOutUrl="/" />
+      </SignedIn>
     </nav>
   )
 }

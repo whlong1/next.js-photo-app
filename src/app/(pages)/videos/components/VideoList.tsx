1335e8e1146a216280b32c586be3a049378d7fe1
whlong1 <whunterlong@gmail.com>
2023-08-02 15:08:30 -0400

    Add video card and initial styling

--- src/app/(pages)/videos/components/VideoList.tsx
+++ src/app/(pages)/videos/components/VideoList.tsx
@@ -3,17 +3,17 @@ import { Video } from '@/types/models'
 
 // Services
 import { fetchVideos } from '@/services/backendServices'
+import VideoCard from './VideoCard'
 
 const VideoList = async () => {
-
   const videos: Video[] = await fetchVideos()
 
-
   return (
-    videos.map((vid: any) => (
-      <p>{vid.title}</p>
-    ))
-
+    <section className="responsive-grid">
+      {videos.map((video: any) => (
+        <VideoCard key={video.id} video={video} />
+      ))}
+    </section>
   )
 }
 

a407b88270b1d0b3b920217af3a8d1cc2321bb99
whlong1 <whunterlong@gmail.com>
2023-08-11 20:25:52 -0400

    Add image upload preview

--- src/app/(pages)/videos/components/MediaUploader.tsx
+++ src/app/(pages)/videos/components/MediaUploader.tsx
@@ -1,21 +1,25 @@
 "use client"
 
+// React
+import { useState } from "react"
+
 // Services
 import { uploadMedia } from "@/services/frontendServices"
 
 const MediaUploader = () => {
+  const [uploadedUrl, setUploadedUrl] = useState("")
 
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0]!
-    const filename = file.name
-    const fileType = file.type
-    const res = await uploadMedia(file, filename, fileType)
-    console.log("Upload response", res)
+    const res = await uploadMedia(file)
+    console.log(res.status)
+    setUploadedUrl(res.uploadedUrl)
   }
 
   return (
     <>
       <h2>Upload Media</h2>
+      {uploadedUrl && <img src={uploadedUrl} alt="Preview" />}
       <input
         type="file"
         onChange={handleChange}

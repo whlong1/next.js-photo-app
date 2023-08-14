bb2e6555486e2206a4cee84072c57b88a4ad104e
whlong1 <whunterlong@gmail.com>
2023-08-14 14:07:13 -0400

    Add initial state for photo form

--- src/app/(pages)/photos/new/components/NewPhotoForm.tsx
+++ src/app/(pages)/photos/new/components/NewPhotoForm.tsx
@@ -1,4 +1,23 @@
+"use client"
+
+// React
+import { useState } from "react"
+
+// Types
+import { PhotoFormData } from "@/types/forms"
+
 const NewPhotoForm = () => {
+  const initialState: PhotoFormData = {
+    title: "",
+    category: "",
+    location: "",
+    description: "",
+    isUploaded: false,
+    year: new Date().getFullYear(),
+  }
+
+  const [formData, setFormData] = useState(initialState)
+
   return (
     <form action="">
 

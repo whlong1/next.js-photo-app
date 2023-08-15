63d6ee538a0f67f210a0ce273db67ce70ff0685e
whlong1 <whunterlong@gmail.com>
2023-08-15 18:50:33 -0400

    Build out photo row with initial styling

--- src/app/(pages)/dashboard/photos/components/PhotoRow.tsx
+++ src/app/(pages)/dashboard/photos/components/PhotoRow.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link"
 import { Photo } from "@/types/models"
 const PhotoRow = ({ photo }: { photo: Photo }) => {
+  console.log("Photo Row:", photo)
   return (
-    <Link className="border" href={`/dashboard/photos/${photo.id}`}>
-      {photo.url && photo.title && <img src={photo.url} alt={photo.title} />}
-      {photo.id}
+    <Link className="flex border items-center w-full h-24" href={`/dashboard/photos/${photo.id}`}>
+      <div className="border w-12">
+        {photo.url && photo.title && <img src={photo.url} alt={photo.title} />}
+      </div>
+      <div className="flex flex-col">
+        <p>{photo.fileName}</p>
+        <p>{photo.fileSize}</p>
+      </div>
+      <input type="checkbox" checked={photo.isUploaded} />
     </Link>
   )
 }

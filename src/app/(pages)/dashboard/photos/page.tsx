12a2f84001de45bd0610891e4bcef0dc3325fad7
whlong1 <whunterlong@gmail.com>
2023-08-15 15:32:45 -0400

    Add photo list to return

--- src/app/(pages)/dashboard/photos/page.tsx
+++ src/app/(pages)/dashboard/photos/page.tsx
@@ -1,8 +1,18 @@
-const DashboardPhotos = () => {
-  // Default index view???
+import { Photo } from "@/types/models"
+import { fetchMyPhotos } from "@/services/userService"
+import Link from "next/link"
+
+const DashboardPhotos = async () => {
+  const photos: Photo[] = await fetchMyPhotos()
+  console.log("MY PHOTOS", photos)
   return (
     <>
       PHOTOS DASHBOARD LANDING
+      {photos.map((photo) => (
+        <Link key={photo.id} href={`/dashboard/photos/${photo.id}`}>
+          {photo.id}
+        </Link>
+      ))}
     </>
   )
 }

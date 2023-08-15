5c3f43111a1c2e2bdc703581fb0c443662179cf3
whlong1 <whunterlong@gmail.com>
2023-08-15 19:16:34 -0400

    Add params to PhotoDetails

--- src/app/(pages)/dashboard/photos/[photoId]/page.tsx
+++ src/app/(pages)/dashboard/photos/[photoId]/page.tsx
@@ -1,4 +1,8 @@
-const PhotoDetails = () => {
+
+interface PhotoDetailsProps { params: { photoId: string } }
+const PhotoDetails = ({ params: { photoId } }: PhotoDetailsProps) => {
+
+  console.log("DETAILS", photoId)
   return (
     <>
       <h1>PHOTO DETAILS</h1>
--- src/app/(pages)/dashboard/photos/components/PhotoRow.tsx
+++ src/app/(pages)/dashboard/photos/components/PhotoRow.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import { Photo } from "@/types/models"
 const PhotoRow = ({ photo }: { photo: Photo }) => {
-  console.log("Photo Row:", photo)
+  // console.log("Photo Row:", photo)
   return (
     <Link className="flex border items-center justify-around w-full h-24" href={`/dashboard/photos/${photo.id}`}>
       <div className="border w-12">
@@ -11,7 +11,7 @@ const PhotoRow = ({ photo }: { photo: Photo }) => {
         <p>{photo.fileName}</p>
         <p>{photo.fileSize}</p>
       </div>
-      <input type="checkbox" checked={photo.isUploaded} />
+      <input type="checkbox" readOnly checked={photo.isUploaded} />
       <p>Privacy</p>
       <p>Delete</p>
       <p>Options</p>

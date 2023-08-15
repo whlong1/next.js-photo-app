e4512453e34eccee6c7dca49787860ce04c4e6c1
whlong1 <whunterlong@gmail.com>
2023-08-15 19:27:41 -0400

    Add fetchPhotoById service function

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -13,3 +13,15 @@ export const fetchMyPhotos = async (): Promise<Photo[]> => {
     throw error
   }
 }
+
+export const fetchPhotoById = async (photoId: string): Promise<Photo> => {
+  try {
+    const res = await fetch(`${BASE_URL}/api/users/me/photos/${photoId}`, {
+      cache: 'no-store',
+      headers: headers(),
+    })
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}

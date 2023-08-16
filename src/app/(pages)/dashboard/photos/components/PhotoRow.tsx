1cdde1f72b6a9633c68e9b798ee8c06c05e2e080
whlong1 <whunterlong@gmail.com>
2023-08-16 14:58:32 -0400

    Build out delete functionality

--- src/app/(pages)/dashboard/photos/components/PhotoRow.tsx
+++ src/app/(pages)/dashboard/photos/components/PhotoRow.tsx
@@ -2,8 +2,12 @@
 import Link from "next/link"
 import { Photo } from "@/types/models"
 
+import { deletePhoto } from "@/services/photoService"
 const RowDeleteButton = ({ photoId }: { photoId: string }) => {
-  const handleDelete = () => { }
+  const handleDelete = async () => {
+    const res = await deletePhoto(photoId)
+    console.log("Component", res)
+  }
   return <button onClick={handleDelete}>DELETE</button>
 }
 
@@ -11,23 +15,35 @@ const RowDeleteButton = ({ photoId }: { photoId: string }) => {
 
 const PhotoRow = ({ photo }: { photo: Photo }) => {
   console.log("Photo Row:", photo.url)
+  const path = `/dashboard/photos/${photo.id}`
+  const containerStyle = `
+    flex 
+    border 
+    items-center 
+    justify-around 
+    w-full 
+    h-24
+  `
 
   return (
-    <Link className="flex border items-center justify-around w-full h-24" href={`/dashboard/photos/${photo.id}`}>
+    <div className={containerStyle}>
       <div className="border w-12">
         {photo.url && photo.title && <img src={photo.url} alt={photo.title} />}
       </div>
       <div className="flex flex-col">
+        <p>{photo.id}</p>
+
         <p>{photo.fileName}</p>
         <p>{photo.fileSize}</p>
       </div>
       <input type="checkbox" readOnly checked={photo.isUploaded} />
-      
+
       <RowDeleteButton photoId={photo.id} />
+      <Link href={path}>VIEW</Link>
 
       <p>Privacy</p>
       <p>Options</p>
-    </Link>
+    </div>
   )
 }
 

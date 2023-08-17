d681df5394af27426906c867d02dada57628d660
whlong1 <whunterlong@gmail.com>
2023-08-17 12:29:15 -0400

    Remove unused img

--- src/app/(pages)/videos/components/VideoCard.tsx
+++ src/app/(pages)/videos/components/VideoCard.tsx
@@ -5,7 +5,7 @@ const VideoCard = ({ video }: { video: Video }) => {
   return (
     <article className="flex flex-col rounded shadow-md overflow-hidden">
       <div>
-        <img className="h-48 w-full bg-black" src={video.thumbnailUrl} alt={video.title} />
+        {/* <img className="h-48 w-full bg-black" src={video.thumbnailUrl} alt={video.title} /> */}
       </div>
       <div className="flex flex-row">
         <h2>{video.title}</h2>

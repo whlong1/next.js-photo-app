2e6d5640e383db2abeacbb1057fbcefab69cd960
whlong1 <whunterlong@gmail.com>
2023-08-21 17:03:51 -0400

    Add notes on action

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -5,6 +5,14 @@ import { generatePresignedGetURL } from "@/lib/aws"
 
 // This action appears to be dynamic (no apparent need for cache management)
 // Question: Will all db operations on the server behave the same way?
+
+// DB operation requires userId, which requires the Clerk auth() hook.
+// If the request is issued from a server component, the headers hook needs
+// to be included on the fetch for the Clerk hook to work. This makes the route
+// dynamic, and opts out of caching. As a result it makes the most sense to do a 
+// direct DB query inside the server component, though taking advantage of the cache 
+// might be preferable eventually.
+
 export const getMyPhotos = async () => {
   try {
     const { userId } = auth()

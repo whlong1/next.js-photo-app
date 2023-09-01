18de658fc1ef024536f15b54930cd62489274a1d
whlong1 <whunterlong@gmail.com>
2023-09-01 17:59:34 -0400

    Lift photo form state to parent

--- src/app/(pages)/dashboard/photos/components/NewPhotoDialog.tsx
+++ src/app/(pages)/dashboard/photos/components/NewPhotoDialog.tsx
@@ -1,21 +1,53 @@
 "use client"
-// React
+
+// Hooks
 import { useState } from "react"
+import { useRouter } from "next/navigation"
 
 // Components
 import NewPhotoForm from "./NewPhotoForm"
 import PhotoUploader from "./PhotoUploader"
 
-const NewPhotoDialog = () => {
+// Types
+import { Photo } from "@/types/models"
+import { PhotoFormData } from "@/types/forms"
 
-  /*/
-    PhotUploader updates the URL params with the new photoId
-    NewPhotoForm requires this URL param (queryParams.get("photoId"))
-    Move up necessary state to NewPhotoDialog
-    Submit form and store file in one action
-    Preview image with local file
-    Only downside is edit will require an adjusted workflow
-  /*/
+// Services
+import { createOrUpdatePhoto } from "@/services/photoService"
+
+/*/
+  PhotUploader updates the URL params with the new photoId
+  NewPhotoForm requires this URL param (queryParams.get("photoId"))
+  Move up necessary state to NewPhotoDialog
+  Submit form and store file in one action
+  Preview image with local file
+  Only downside is edit will require an adjusted workflow
+/*/
+
+const NewPhotoDialog = () => {
+  const initialState: PhotoFormData = {
+    title: "",
+    category: "",
+    location: "",
+    description: "",
+    year: new Date().getFullYear(),
+  }
+
+  const router = useRouter()
+  const [formData, setFormData] = useState(initialState)
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    // const photoData: Photo = await createOrUpdatePhoto(photoId, formData)
+    router.refresh()
+  }
+
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type } = target
+    setFormData({
+      ...formData, [name]: type === "number" ? parseInt(value) : value
+    })
+  }
 
   return (
     <div className="bg-white border rounded">
@@ -24,8 +56,14 @@ const NewPhotoDialog = () => {
         <button>X</button>
       </header>
       <section className="flex h-[380px] items-stretch">
-        <PhotoUploader />
-        <NewPhotoForm />
+        <PhotoUploader 
+        
+        />
+        <NewPhotoForm
+          formData={formData}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+        />
       </section>
     </div>
   )
--- src/app/(pages)/dashboard/photos/components/NewPhotoForm.tsx
+++ src/app/(pages)/dashboard/photos/components/NewPhotoForm.tsx
@@ -1,72 +1,15 @@
-"use client"
-
-// Hooks
-import { useState } from "react"
-import { useRouter } from "next/navigation"
-import { useQueryManager } from "@/hooks/useQueryManager"
-
 // Types
-import { Photo } from "@/types/models"
 import { PhotoFormData } from "@/types/forms"
 
-// Services
-import { createOrUpdatePhoto } from "@/services/photoService"
+interface NewPhotoFormProps {
+  formData: PhotoFormData;
+  handleSubmit: (e: React.FormEvent) => Promise<void>;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-const NewPhotoForm = () => {
-  const initialState: PhotoFormData = {
-    title: "",
-    category: "",
-    location: "",
-    description: "",
-    year: new Date().getFullYear(),
-  }
-
-  const router = useRouter()
-  const [formData, setFormData] = useState(initialState)
-  const { queryParams, setQueryParams } = useQueryManager()
-
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    // This is very unreliable, because the params might be set for something old!!!
-    const photoId = queryParams.get("photoId")
-    console.log("FORM", queryParams.get("photoId"))
-    if (!photoId) {
-      console.log("PLEASE UPLOAD A FILE FIRST")
-      return
-    }
-
-    // Check upload status, add isUploaded prop to formData
-    // Need to set formState with updatedPhotoData for editing
-    // In route handler, instead of finding existing, could create new resource
-    // based on isUploaded boolean.
-    // Uploads should be handled in dashboard - better state management
-    // React query cache?
-    // fetch photos and videos: dynamic segments for users
-
-    // drag and drop quandry, if the image exists, display the image as a row with the data
-    // allow the user to delete it and upload another
-
-    // What if a user wants to submit the upload and form at the same time?
-    // Add condition to include additional form data in route handler??
-
-    // How can you pass photoId before it exists? Silly!
-    const photoData: Photo = await createOrUpdatePhoto(photoId, formData)
-    setQueryParams("photoId", photoData.id)
-    console.log("Photo Data", photoData)
-    router.refresh()
-  }
-
-  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, type } = target
-    setFormData({
-      ...formData, [name]: type === "number" ? parseInt(value) : value
-    })
-  }
-
+const NewPhotoForm = ({ formData, handleSubmit, handleChange }: NewPhotoFormProps) => {
   const inputClassNames = "border border-slate-200 text-slate-300 p-1 text-xs"
   const labelClassNames = "text-slate-300 font-medium text-xs mb-1"
-
 
   return (
     <form className="w-[50%] h-full flex flex-col justify-between p-4" onSubmit={handleSubmit}>

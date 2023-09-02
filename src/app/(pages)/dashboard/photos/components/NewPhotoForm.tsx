3723cdb2e00537aa1f522704f4ff2bc894616daf
whlong1 <whunterlong@gmail.com>
2023-09-02 18:13:39 -0400

    Update value attributes

--- src/app/(pages)/dashboard/photos/components/NewPhotoForm.tsx
+++ src/app/(pages)/dashboard/photos/components/NewPhotoForm.tsx
@@ -2,12 +2,12 @@
 import { PhotoFormData } from "@/types/forms"
 
 interface NewPhotoFormProps {
-  formData: PhotoFormData;
+  photoFormData: PhotoFormData;
   handleSubmit: (e: React.FormEvent) => Promise<void>;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const NewPhotoForm = ({ formData, handleSubmit, handleChange }: NewPhotoFormProps) => {
+const NewPhotoForm = ({ photoFormData, handleSubmit, handleChange }: NewPhotoFormProps) => {
   const inputClassNames = "border border-slate-200 text-slate-300 p-1 text-xs"
   const labelClassNames = "text-slate-300 font-medium text-xs mb-1"
 
@@ -20,7 +20,7 @@ const NewPhotoForm = ({ formData, handleSubmit, handleChange }: NewPhotoFormProp
           type="text"
           id="title"
           name="title"
-          value={formData.title}
+          value={photoFormData.title}
           onChange={handleChange}
           className={inputClassNames}
         />
@@ -33,7 +33,7 @@ const NewPhotoForm = ({ formData, handleSubmit, handleChange }: NewPhotoFormProp
           type="text"
           id="category"
           name="category"
-          value={formData.category}
+          value={photoFormData.category}
           onChange={handleChange}
           className={inputClassNames}
         />
@@ -46,7 +46,7 @@ const NewPhotoForm = ({ formData, handleSubmit, handleChange }: NewPhotoFormProp
           type="text"
           id="location"
           name="location"
-          value={formData.location}
+          value={photoFormData.location}
           onChange={handleChange}
           className={inputClassNames}
         />
@@ -59,7 +59,7 @@ const NewPhotoForm = ({ formData, handleSubmit, handleChange }: NewPhotoFormProp
           type="text"
           id="description"
           name="description"
-          value={formData.description}
+          value={photoFormData.description}
           onChange={handleChange}
           className={inputClassNames}
         />
@@ -72,7 +72,7 @@ const NewPhotoForm = ({ formData, handleSubmit, handleChange }: NewPhotoFormProp
           type="number"
           id="year"
           name="year"
-          value={formData.year}
+          value={photoFormData.year}
           onChange={handleChange}
           className={inputClassNames}
         />

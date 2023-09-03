c53ef0e8f9d01c7476fc5d65d05c70b2ed8e6011
whlong1 <whunterlong@gmail.com>
2023-09-03 16:23:45 -0400

    Clean up styling

--- src/app/(routes)/dashboard/photos/page.tsx
+++ src/app/(routes)/dashboard/photos/page.tsx
@@ -10,20 +10,9 @@ import PhotoRow from "./components/PhotoRow/PhotoRow"
 const DashboardPhotos = async () => {
   const myPhotos: Photo[] = await getMyPhotos()
 
-  const photoDashboardHeaderClassNames = `
-    p-4 
-    h-[45px]
-    flex 
-    w-full 
-    text-xs
-    border-b
-    items-center
-    bg-slate-100
-  `
-
   return (
     <>
-      <header className={photoDashboardHeaderClassNames}>
+      <header className="p-4 h-[45px] flex w-full text-xs border-b items-center bg-slate-100">
         <p>File</p>
         <p>Public</p>
         <p>Date Uploaded</p>

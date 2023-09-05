15e93753c5192d6ed68209ee9129a222a556af41
whlong1 <whunterlong@gmail.com>
2023-09-05 14:15:47 -0400

    Resolve drag and drop event propagation bug

--- src/app/(routes)/dashboard/photos/components/NewPhotoDialog/FileInput.tsx
+++ src/app/(routes)/dashboard/photos/components/NewPhotoDialog/FileInput.tsx
@@ -1,4 +1,11 @@
-const FileInput = ({ selectAndPreview }: { selectAndPreview: (file: File) => void; }) => {
+interface FileInputProps {
+  selectAndPreview: (file: File) => void;
+  setIsDragActive: (bool: boolean) => void;
+  handleDrop: (e: React.DragEvent<HTMLDivElement | HTMLLabelElement>) => void;
+}
+const FileInput = (props: FileInputProps) => {
+  const { selectAndPreview, handleDrop, setIsDragActive } = props
+
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0]
     if (file) selectAndPreview(file)
@@ -13,7 +20,13 @@ const FileInput = ({ selectAndPreview }: { selectAndPreview: (file: File) => voi
         id="hiddenFileInput"
         onChange={handleChange}
       />
-      <label className="btn btn-slate" htmlFor="hiddenFileInput">
+      <label
+        onDrop={handleDrop}
+        onDragOver={(e) => { e.preventDefault(); setIsDragActive(true) }}
+        onDragEnter={(e) => { e.preventDefault(); setIsDragActive(true) }}
+        onDragLeave={(e) => { e.preventDefault(); e.stopPropagation() }}
+        className="btn btn-slate pointer-events-auto" htmlFor="hiddenFileInput"
+      >
         SELECT FILE
       </label>
     </>

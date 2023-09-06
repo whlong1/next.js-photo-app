5b6fa8d4be28520e255e1a0ced533beabe1d0857
Hunter Long <whunterlong@gmail.com>
2023-09-06 18:51:51 -0400

    Add slide out to browse layout

--- src/app/(routes)/photos/layout.tsx
+++ src/app/(routes)/photos/layout.tsx
@@ -1,10 +1,34 @@
 // Components
+import Menu from "@/components/Menu"
 import FilterNav from "./components/FilterNav"
+import FilterSection from "./components/FilterSection"
+
+// Constants
+import { PHOTO_CATEGORIES, ASPECT_RATIOS } from "@/lib/constants"
+
+// Types
+import { MenuElements } from "@/types/elements"
 
 const PhotosLayout = ({ children }: { children: React.ReactNode }) => {
+  const menuElements: MenuElements = {
+    name: <h2 className="text-sm">Browse</h2>,
+    content: (
+      <>
+        <FilterSection
+          queryKey="category"
+          queryValues={PHOTO_CATEGORIES}
+        />
+        <FilterSection
+          queryKey="Aspect Ratio"
+          queryValues={ASPECT_RATIOS}
+        />
+      </>
+    )
+  }
+
   return (
     <main className="flex">
-      <FilterNav />
+      <Menu menuElements={menuElements} />
       {children}
     </main>
   )

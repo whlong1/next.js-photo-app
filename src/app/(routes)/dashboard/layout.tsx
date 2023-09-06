09e859c7ac706cf61e625dc59f2be8cb4bd8f9d2
Hunter Long <whunterlong@gmail.com>
2023-09-06 18:22:45 -0400

    Add dashboard toggle

--- src/app/(routes)/dashboard/layout.tsx
+++ src/app/(routes)/dashboard/layout.tsx
@@ -1,18 +1,9 @@
-import Link from "next/link"
+import Menu from "./components/Menu/Menu"
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex">
-      <nav className="side-nav">
-        <header className="header">
-          <Link href="/dashboard">
-            <h2 className="text-sm">Dashboard</h2>
-          </Link>
-        </header>
-        <Link className="nav-item" href="/dashboard/account">Account</Link>
-        <Link className="nav-item" href="/dashboard/photos">Photos</Link>
-        <Link className="nav-item" href="/dashboard/favorites">Favorites</Link>
-        <Link className="nav-item" href="/dashboard/favorites">Analytics</Link>
-      </nav>
+      <Menu />
       <section className="flex flex-col w-full">
         {children}
       </section>

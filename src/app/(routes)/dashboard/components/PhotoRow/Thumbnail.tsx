05c95c7c7f8d561a2b5925f203b9d0c3cad821d5
Hunter Long <whunterlong@gmail.com>
2023-09-08 13:27:04 -0400

    Refactor thumbnail for initial color swatch loading bg

--- src/app/(routes)/dashboard/components/PhotoRow/Thumbnail.tsx
+++ src/app/(routes)/dashboard/components/PhotoRow/Thumbnail.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+// Hooks
+import { useState, useEffect } from "react"
+
 // Types
 import { Photo } from "@/types/models"
 
@@ -9,17 +12,25 @@ import Image from "next/image"
 // Credit for the smooth Image loading idea:
 // https://www.youtube.com/watch?v=GG66vQgc1Vg&ab_channel=SakuraDev
 
+// Doc on hexadecimal color code transparency:
+// https://gist.github.com/lopspower/03fb1cc0ac9f32ef38f4
+
+// Note: Including the unoptimized prop on Image seems to improve loading time
+// Might be due to the fact that it's directly fetched from the source (S3)
+
 const Thumbnail = ({ photo }: { photo: Photo }) => {
+  const { hex } = photo
   const imageInfo = photo.url
     ? { src: photo.url, alt: "Thumbnail" }
     : { src: "/assets/placeholder.png", alt: "No image source" }
 
+  // Default of #e2e8f0 is equivalent to slate-200:
+  const [backgroundColor, setBackgroundColor] = useState("#e2e8f0")
 
-  // Note: Including the unoptimized prop seems to improve loading time
-  // as it's directly fetched from the source (S3)
+  useEffect(() => { setBackgroundColor(`${hex}99`) }, [hex])
 
   return (
-    <div className="thumbnail">
+    <div className="thumbnail transition-colors duration-500 ease-in-out" style={{ backgroundColor: backgroundColor }}>
       <Image
         fill={true}
         unoptimized={true}

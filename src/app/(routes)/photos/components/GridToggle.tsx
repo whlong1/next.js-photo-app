4feb36fd8266843dea4bd86a283bb9c481385b67
Hunter Long <whunterlong@gmail.com>
2023-09-16 14:17:50 -0400

    Add conditional styling for active filter

--- src/app/(routes)/photos/components/GridToggle.tsx
+++ src/app/(routes)/photos/components/GridToggle.tsx
@@ -6,16 +6,20 @@ import { useQueryManager } from "@/hooks/useQueryManager"
 const GridToggle = () => {
   const { queryParams, setQueryParams } = useQueryManager()
   const isMasonry = queryParams.get("masonry") === "true"
-  // const isBasic = queryParams.get("masonry") = "false"
-
-  console.log(isMasonry)
-
   const handleClick = () => setQueryParams("masonry", "true")
 
+  const baseClass = "w-full h-full hover:bg-fill-grey"
+  const activeClass = "bg-fill-grey"
+
   return (
-    <div>
-      <button onClick={handleClick}>[B]</button>
-      <button onClick={handleClick}>[M]</button>
+    <div className="w-20 ml-4 flex justify-center border rounded-full" onClick={handleClick}>
+      <button className={`${baseClass} ${isMasonry ? activeClass : ""}  rounded-l-full`}>
+        x
+      </button>
+      <div className="border-r"></div>
+      <button className={`${baseClass} ${isMasonry ? "" : activeClass} rounded-r-full`}>
+        x
+      </button>
     </div>
   )
 }

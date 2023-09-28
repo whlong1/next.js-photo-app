a4663476ca43a9336ea68ae5352b2a17e45c3bef
whlong1 <whunterlong@gmail.com>
2023-09-28 16:20:08 -0400

    Clean up code

--- src/app/api/photos/helpers/queryHelpers.ts
+++ src/app/api/photos/helpers/queryHelpers.ts
@@ -1,8 +1,7 @@
 import { ValidParams } from "@/types/enums"
 import { ORCondition, PrismaQueryObject } from "@/types/types"
 
-// =================== CONSTANTS ====================
-
+// Used to exclude certain fields from a generalized search.
 const EXCLUDED_PARAMS = [
   ValidParams.KEYWORD,
   ValidParams.YEAR,
@@ -10,26 +9,12 @@ const EXCLUDED_PARAMS = [
   ValidParams.HUE_RANGE
 ]
 
-// =================== UTILITY ====================
-/**
-  Filters the given URL search parameters based on the defined valid parameters.
-  @param pair The pair of [key, value] representing a URL search parameter.
-  @returns {boolean} Whether the given pair's key is a valid parameter.
-*/
-
+// Filters the given URL search parameters based on the defined valid parameters.
 const paramFilterFn = (pair: [string, string]) => {
   return Object.values(ValidParams).includes(pair[0] as ValidParams)
 }
 
-
 // =================== QUERY FUNCTIONS ====================
-
-/**
-  Creates a Prisma query object based on the given keyword.
-  The returned object contains OR conditions for each valid parameter.
-  @param keyword The keyword used to filter the search results.
-  @returns {ORCondition} The constructed Prisma OR condition object.
-*/
 
 const createHueRangeQuery = (hueRange: string[]) => {
   if (hueRange.length !== 2) return {}
@@ -41,55 +26,43 @@ const createMimeTypeQuery = (mimeType: string) => {
   return { mimeType: `image/${mimeType.toLowerCase()}` }
 }
 
-// Wonky way to handle a boolean, but I think brightness makes
-// more sense as a query param for users over "isDark"
-// Consider updating the Photo schema
 const createBrightnessQuery = (brightness: string) => {
   if (brightness !== "light" && brightness !== "dark") return {}
   return { isDark: brightness === "dark" }
 }
 
-// Handles general search term query:
-const createORCondition = (keyword: string): ORCondition => {
-  // Valid keys array ("keyword" and "year" must be excluded)
+const createKeywordSearchCondition = (keyword: string): ORCondition => {
   const validKeys = Object.values(ValidParams).filter(
     (key) => !EXCLUDED_PARAMS.includes(key)
   )
 
-  // Produce Prisma filter conditions from valid keys
   const filterConditionsArray = validKeys.map((paramKey) => {
     return { [paramKey]: { contains: keyword, mode: "insensitive" } }
   })
-  // Object formatted for Prisma
+
   return { OR: filterConditionsArray }
 }
 
 // =================== MAIN EXPORT ====================
 
-/**
-  Creates a Prisma query object based on the given URL's search parameters.
-  @param url The URL containing the search parameters.
-  @returns {PrismaQueryObject} The constructed Prisma query object.
-*/
-
+// Creates a Prisma query object based on the given URL's search parameters.
 export const createPrismaQueryFromURL = (url: string): PrismaQueryObject => {
   const { searchParams } = new URL(url)
-  // Filter url based on valid keys (Example: [ "category", "people" ])
+  // Filters url based on valid keys.
   const validPairs = Array.from(searchParams).filter((paramFilterFn))
 
-  // Convert to object (Example: { category: "people" })
+  // Converts array to an object (Example: { category: "people" })
   const searchParamsObject = Object.fromEntries(validPairs)
 
-  // Separate keyword prop from the rest of the search object.
+  // Destructures searchParamsObject for individualized checks
   const { keyword, hueRange, brightness, mimeType, ...rest } = searchParamsObject
 
-  // Handle general keyword search with OR condition.
   return {
     ...rest,
     isPublic: true,
-    ...(keyword && createORCondition(keyword)),
     ...(mimeType && createMimeTypeQuery(mimeType)),
     ...(brightness && createBrightnessQuery(brightness)),
-    ...(hueRange && createHueRangeQuery(hueRange.split('-')))
+    ...(hueRange && createHueRangeQuery(hueRange.split('-'))),
+    ...(keyword && createKeywordSearchCondition(keyword)),
   }
 }

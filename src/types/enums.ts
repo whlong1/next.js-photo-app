b759dbfea2fb28b02185a118570e53f9ca46ad8b
whlong1 <whunterlong@gmail.com>
2023-09-28 13:19:26 -0400

    Add enums.ts

---
+++ src/types/enums.ts
@@ -0,0 +1,13 @@
+export enum ValidParams {
+  YEAR = "year",
+  KEYWORD = "keyword",
+  CATEGORY = "category",
+  LOCATION = "location",
+  AUTHOR_NAME = "authorName",
+  DESCRIPTION = "description",
+  ASPECT_RATIO = "aspectRatio",
+  DOMINANT_COLOR = "dominantColor",
+  BRIGHTNESS = "brightness",
+  HUE_RANGE = "hueRange",
+  MIME_TYPE = "mimeType",
+}
